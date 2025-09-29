da48255bc2a82fbc48ab5879513d0e8a5d471a7b
Helloyunho <me@helloyunho.xyz>
2025-09-30 01:14:49 +0900

    ⬆️ chore: upgrade platform support

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "Harmonia",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+        .macCatalyst(.v16),
+        .tvOS(.v16),
+        .watchOS(.v9),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
